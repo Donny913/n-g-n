b8bb78d6790e55f4784e4ba91dfbee0855ef5c8c
Oleg Miniuk <oleg.miniuk@gmail.com>
2018-03-31 00:06:08 +0300

    deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies*/
+/* eslint-disable no-console*/
+const fs = require('fs-extra');
+const path = require('path');
+
+const buildPath = path.join(__dirname, '../build');
+const publicPath = path.join(__dirname, '../../g-news-app/public');
+
+if (fs.existsSync(publicPath)) {
+  fs.remove(publicPath, err => {
+    if (err) {
+      console.error(err);
+    } else {
+      fs.rename(buildPath, publicPath, errror => console.error(errror));
+    }
+  });
+}
+
