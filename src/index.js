ce7360225e5c815603517cca6781d6b1a4c4d030
oleg.miniuk@gmail.com <oleg.miniuk@gmail.com>
2018-04-06 17:50:00 +0300

    no service workers

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles/css/index.css';
 import { initStyles } from './styles/initStyles';
 import App from './launcher/App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 initStyles();
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
