5287ae26fb927087776038598c938d5798efab61
oleg.miniuk@gmail.com <oleg.miniuk@gmail.com>
2018-04-12 15:10:24 +0300

    deploy:: fixed im folder is not empty

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,13 +7,19 @@ const { exec } = require('child_process');
 const buildPath = path.join(__dirname, '../build');
 const publicPath = path.join(__dirname, '../../g-news-app/public');
 
+const removeUiBuildToApp = () => {
+  fs.rename(buildPath, publicPath, error => console.error(error));
+};
+
 exec('npm run build', () => {
   if (fs.existsSync(publicPath)) {
     fs.remove(publicPath, err => {
       if (err) {
         console.error(err);
       }
+      removeUiBuildToApp();
     });
+  } else {
+    removeUiBuildToApp();
   }
-  fs.rename(buildPath, publicPath, error => console.error(error));
 });
